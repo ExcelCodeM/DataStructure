d0c90428300333d5d436565e353363ce6fb6079b
ExcelCodeM <1446896556@qq.com>
2020-05-26 23:47:54 +0800

    a

---
+++ src/main/java/com/csdn/dataStructure/netty/netty/NettyServer.java
@@ -0,0 +1,56 @@
+package com.csdn.dataStructure.netty.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author ：Breeze
+ * @date ：Created in 2020/5/26 22:09
+ * @description：
+ */
+public class NettyServer {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //创建两个线程组，bossGroup和workerGroup，bossGroup只接受accept事件，
+        //并把与客户端相关联的channel注册到workerGroup上， workerGroup只关注read/write事件
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        //服务器端的启动对象，配置参数
+        ServerBootstrap bootstrap = new ServerBootstrap();
+
+        try {
+            bootstrap.group(bossGroup, workerGroup)     //设置两个线程组
+                    .channel(NioServerSocketChannel.class)      //使用NioServerSocketChannel作为服务器的通道类型
+                    .option(ChannelOption.SO_BACKLOG, 128)      //设置线程队列得到连接的个数
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)      //设置保持活动连接状态
+                    .childHandler(new ChannelInitializer<SocketChannel>() {     //创建一个通道测试对象
+
+                        //给pipeline设置处理器
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new NioServerHandle());
+                        }
+
+                    });     //给我们的workerGroup设置handle处理器
+
+            //绑定监听端口并同步，返回ChannelFuture对象
+            //执行即启动服务器
+            ChannelFuture channelFuture = bootstrap.bind(6668).sync();
+
+            //对关闭通道进行监听
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+}
---
+++ src/main/java/com/csdn/dataStructure/netty/netty/NioServerHandle.java
@@ -0,0 +1,58 @@
+package com.csdn.dataStructure.netty.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author ：Breeze
+ * @date ：Created in 2020/5/26 23:00
+ * @description：
+ */
+
+/**
+ * 说明：
+ * 1、我们自定义的handle需要继承netty提供的HandleAdapter（规范），此时，我们的handle才可以成为一个handle
+ */
+public class NioServerHandle extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 读取数据方法（读取客户端发送的消息）
+     *
+     * @param ctx 上下文对象，可以获取 管道 channel、 通道 pipeline、 地址 等
+     * @param msg 客户端发送的数据， Object对象
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        System.out.println("server ctx = " + ctx);
+        ByteBuf byteBuf = (ByteBuf) msg;    //netty提供的buffer缓冲区
+        System.out.println("客户端发送的数据是：" + byteBuf.toString(CharsetUtil.UTF_8));
+        System.out.println("客户端地址是：" + ctx.channel().remoteAddress());
+
+    }
+
+    /**
+     * 读取完客户端发送数据执行的方法
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello 客户端", CharsetUtil.UTF_8));
+
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+        ctx.channel().close();
+
+    }
+}
